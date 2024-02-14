5bf49396d9308b6739cfdb53934e2cfa248f82cf
Oori Mark <oorimark@gmail.com>
2024-02-14 14:41:36 +0100

    Starting chapter 3

---
+++ Common-programming-concepts/src/5-data_types.rs
@@ -0,0 +1,28 @@
+fn main(){
+
+    // Signed Integers
+    let x: i8 = -3;
+    let x: i32 = -3;
+    let x: i64 = -3;
+    let x: i128 = -3;
+    let x: isize = -3;
+
+    // Unsigned Integers
+    let x: u8 = 3;
+    let x: u32 = 3;
+    let x: u64 = 3;
+    let x: u128 = 3;
+    let x: usize = 3;
+
+    // Floating f64 and f32 
+    // NB: It only supports signed floating point
+    let x: f32 = 24.2;
+    let x: f64 = 24.2;
+
+    // Boolean Type
+    let x: bool = true;
+    let x: bool = false;
+
+    // Character Type
+    let c: char = 'z';
+}
